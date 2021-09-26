f53ade581776b0d5008fe9c82c6c95a833b3c0ed
xiezx23 <xiezx23@mail2.sysu.edu.cn>
2021-09-26 20:38:05 +0800

    v1.1

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,12 @@
+/*************************************************************************
+	> File Name: hello_world.cpp
+	> Author: ZX Xie
+	> Mail: 1514806752@qq.com
+	> Created Time: 2021年09月26日 
+ ************************************************************************/
+
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
