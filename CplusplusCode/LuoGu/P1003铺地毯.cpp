4fda9a12801d7b3c6bef731eb7d7eef936dfbf4d
xiezx23 <xiezx23@mail2.sysu.edu.cn>
2021-10-31 13:56:11 +0800

    21-10-31

---
+++ CplusplusCode/LuoGu/P1003铺地毯.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <queue>
+#define ll long long
+#define addfor(i, n) for (int i = 0; i < n; ++i)
+#define subfor(i, n) for (int i = n-1; i >= 0; --i)
+#define MAXNUM 100
+#define mod 80112002
+using namespace std;
+
+typedef struct node{
+    int a, b;
+    int g, k;
+}node;
+
+int main () {
+    int n, a, b, g, k;
+    cin >> n;
+    node arr[n];
+    addfor(i, n) {
+        scanf("%d%d%d%d",&a,&b,&g,&k);
+        arr[i].a = a;
+        arr[i].b = b;
+        arr[i].g = g;
+        arr[i].k = k;
+    }
+    int x, y;
+    scanf("%d%d",&x,&y);
+    subfor(i, n) {
+        if (arr[i].a <= x && arr[i].a+arr[i].g >= x) {
+            if (arr[i].b <= y && arr[i].b+arr[i].k >= y){
+                printf("%d\n",i+1);
+                return 0;
+            }
+        }
+    }
+    cout << "-1";
+    return 0;
+}
---
+++ CplusplusCode/LuoGu/P4017最大食物链计数.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <queue>
+#define ll long long
+#define addfor(i, n) for (int i = 0; i < n; ++i)
+#define subfor(i, n) for (int i = n-1; i >= 0; --i)
+#define MAXNUM 100
+#define mod 80112002
+using namespace std;
+
+typedef struct node
+{
+    int num;
+    vector<node*> in;
+    vector<node*> out;
+    node() {num = -1;}
+}node;
+
+node arr[5001];
+
+ll dfs(node* n){
+    ll ans = 0;
+    if (n->num != -1) return n->num;
+    if (n->out.size() == 0) return 1;
+    else {
+        for (auto i: n->out) {
+            ans += dfs(i);
+            ans %= mod;
+        }
+    }
+    n->num = ans;
+    return ans;
+}
+
+int main() {
+    int n, m;
+    int x,y;
+    scanf("%d%d",&n,&m);
+    addfor(i, m){
+        scanf("%d%d",&x,&y);
+        arr[x].out.push_back(&arr[y]);
+        arr[y].in.push_back(&arr[x]);
+    }
+    ll ans = 0;
+    for (auto i: arr) {
+        if (i.in.size() == 0 && i.out.size() != 0) {
+            ans += dfs(&i);
+            ans %= mod;
+        }
+    }
+    cout << ans;
+    return 0;
+}
